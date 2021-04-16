79dc21ab915865c474d441899932ec948ea14a96
PabloBrianese <pablobrianese@gmail.com>
2021-04-16 20:48:55 -0300

    cambios menores

--- practica/_scripts/ejercicio_2/imagenes_variantes.sh
+++ practica/_scripts/ejercicio_2/imagenes_variantes.sh
@@ -5,4 +5,4 @@ convert ./datos/Ej2_aux.jpg -quality 1 ./datos/Ej2_aux_quality.jpg
 convert ./datos/Ej2_aux.jpg -vignette 200x200 ./datos/Ej2_aux_vignette.jpg
 
 # Cambiamos la imagen original para simular un dibujo en carbonilla
-convert ./datos/Ej2_aux.jpg -charcoal 1 ./datos/Ej2_aux_quality.jpg
+convert ./datos/Ej2_aux.jpg -charcoal 1 ./datos/Ej2_aux_charcoal.jpg
