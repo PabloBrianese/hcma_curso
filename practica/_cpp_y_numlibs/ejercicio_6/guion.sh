0a97ebdf985349aa9bffc0cadd78021e03bddac2
PabloBrianese <pablobrianese@gmail.com>
2021-05-11 17:55:36 -0300

    Ejercicio 5-6, con salida a un archivo de texto

--- practica/_cpp_y_numlibs/ejercicio_6/guion.sh
+++ practica/_cpp_y_numlibs/ejercicio_6/guion.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 g++ probagsl.cpp -o probagsl.out -lgsl -lgslcblas -lm
+
+./probagsl.out 10
--- practica/_cpp_y_numlibs/ejercicio_6/probagsl.cpp
+++ practica/_cpp_y_numlibs/ejercicio_6/probagsl.cpp
@@ -1,56 +1,53 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <stdlib.h>
-#include <gsl/gsl_rng.h>
-#include <gsl/gsl_randist.h>
+#include <cstdlib>  /* exit, EXIT_FAILURE */
+#include <fstream> /* ofstream */
+#include <string>  /* string */
+#include <vector>  /* vector */
+#include <gsl/gsl_rng.h>  /* gsl_rng_type, gsl_rng, gsl_rng_env_setup, gsl_rng_default, gsl_rng_default_seed, gsl_rng_alloc, gsl_rng_free, gsl_rng_set */
+#include <gsl/gsl_randist.h>  /* gsl_ran_gaussian, gsl_ran_lognormal */
 
-double aleatorio_preprocesador (gsl_rng*, const std::string);
+using namespace std;
 
+// Distribución de probabilidad a muestrear
 // "gsl_ran_gaussian" |--> normal
 // "gsl_ran_lognormal" |--> lognormal
 #define DIRECTIVA "gsl_ran_gaussian"
 
+double aleatorio_preprocesador(gsl_rng*, const string);
+
 int main(int argc, char* argv[]) {
-    // Número de puntos aleatoios a generar
-    if (argc == 1) {
-	std::cout << "Ingresar un número de muestras a tomar"
-	          << std::endl; 
-        return 0;
-    }
+    if (argc != 2) { exit(EXIT_FAILURE); }
 
-    const unsigned N = (unsigned) atoi(argv[1]);
-    // puntero al generador de números pseudoaleatorios
-    const gsl_rng_type* T;
-    gsl_rng* r;
+    // Número de muestras a generar
+    const int N = atoi(argv[1]);
 
-    gsl_rng_env_setup();
+    // Tipo de generador
+    const gsl_rng_type* T = gsl_rng_default;  // = gsl_rng_mt1997
+    // Puntero al generador
+    gsl_rng* r = gsl_rng_alloc(T);
+    // Semilla del generador
+    gsl_rng_set(r, gsl_rng_default_seed);  // = 0
 
-    T = gsl_rng_default;
-    r = gsl_rng_alloc (T);
+    // Muestreo
+    vector<double> ys;
+    for (int i = 0; i < N; ++i)
+        ys.push_back(aleatorio_preprocesador(r, DIRECTIVA));
+
+    // Se libera la memoria usada por el generador
+    gsl_rng_free(r);
 
     // Archivo de datos
-    std::ofstream datos;
-    datos.open ("muestreo_" DIRECTIVA ".dat");
-    for (unsigned i = 0; i < N; ++i) {
-        datos << aleatorio_preprocesador(r, DIRECTIVA)
-	      << std::endl;
-    }
+    ofstream datos;
+    datos.open("muestreo_" DIRECTIVA ".dat");
+    for (double y : ys) datos << y << endl;
     datos.close();
-
-    gsl_rng_free (r);
-
-    return 0;
 }
 
-double aleatorio_preprocesador (gsl_rng* r, const std::string distribution) {
+double aleatorio_preprocesador (gsl_rng* r, const string distribution) {
     if (distribution == "gsl_ran_gaussian") {
         return gsl_ran_gaussian(r, 1.0);
     }
     else if (distribution == "gsl_ran_lognormal") {
         return gsl_ran_lognormal(r, 0.0, 1.0);
     }
-    else {
-        exit(EXIT_FAILURE);
-    }
+    else { exit(EXIT_FAILURE); }
 }
