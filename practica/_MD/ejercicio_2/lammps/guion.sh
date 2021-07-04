042d8306286520311e697252d25ed91d20b71847
PabloBrianese <pablobrianese@gmail.com>
2021-07-04 02:47:50 -0300

    Pasar campos E y B por terminal de comandos

--- practica/_MD/ejercicio_2/lammps/guion.sh
+++ practica/_MD/ejercicio_2/lammps/guion.sh
@@ -7,6 +7,9 @@
 #lammps19 -var extern valor -sf opt < ElectronEnCampoEyB.lammps
 
 
-lmp_ubuntu < electronEnCampoElectromagnetico.lammps
+lmp_ubuntu \
+  -v Ex 0 -v Ey 0.1 -v Ez 0 \
+  -v Bx 0 -v By 1 -v Bz 0 \
+  -in electronEnCampoElectromagnetico.lammps
 
 gnuplot -e "title='Simulación con LAMMPS'" ../electronEnCampoElectromagnetico.p
