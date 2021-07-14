f536e037a42d149cede401d84584a61d8c018402
PabloBrianese <pablobrianese@gmail.com>
2021-07-14 13:54:03 -0300

    Revert "Separar el guión del Argón y el del Helio"
    This reverts commit bbb980ffb9a102eeadf7e19632e8e0c99f3ee14f.

--- practica/_MD/ejercicio_3/guion.sh
+++ practica/_MD/ejercicio_3/guion.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/bash
 
-#Correr con 
-#mpirun -np 2 lammps19 -sf opt < ArgonLJ.lammps
-#si se quiere usar la definicion de una variable  desde linea de
-#comandos usar:
-#lammps19 -var extern valor -sf opt < ArgonLJ.lammps
-
-
-lmp_ubuntu -in ArgonLJ.lammps
+lmp_ubuntu \
+  -log none\
+  -in ArgonLJ.lammps
