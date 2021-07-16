99dbb60cdab2e83983467406d0be1406cda230a0
PabloBrianese <pablobrianese@gmail.com>
2021-07-16 18:16:43 -0300

    Solución numérica de ejemplo simple por el método de Diferencias Finitas

---
+++ practica/_OF/finiteDifferences.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <array>
+#include <string>
+
+using namespace std;
+
+constexpr auto filename = "Datos.dat";
+constexpr auto NumIntervalos = 10;
+constexpr auto NumNodos = NumIntervalos + 1;
+constexpr auto DeltaX = 1.0 / double(NumIntervalos);
+
+int main()
+{
+  array<double, NumNodos> y;
+  y[0] = 5;  // condición inicial
+  for (auto i = 1; i < NumNodos; ++i)
+    y[i] = y[i - 1] / (1 + 3 * DeltaX);  // solución iterariva para el resto de los nodos
+
+
+  ofstream Outfile (filename);
+  Outfile << "# x_node y(x_node)" << endl;
+  for (auto i = 0; i < NumNodos; ++i)
+    Outfile << i * DeltaX << " " << y[i] << endl;
+
+  cout << "Listo, solución por diferencias finitas guardadas en el archivo "
+    << filename
+    << endl;
+}
---
+++ practica/_OF/finiteDifferences.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+g++ finiteDifferences.cpp -o finiteDifferences.out -lm
+
+./finiteDifferences.out
