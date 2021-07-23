fd4ee41925df3f4590206014d7607e1e9f536eb7
PabloBrianese <pablobrianese@gmail.com>
2021-07-23 13:59:46 -0300

    Solución numérica con series de Fourier y gráficas

--- practica/_OF/ejercicio_2/fourierSeriesSolution/fourierSeriesSolution.cpp
+++ practica/_OF/ejercicio_2/fourierSeriesSolution/fourierSeriesSolution.cpp
@@ -6,7 +6,7 @@ constexpr auto order = 100;
 constexpr auto Lx = 20.0;
 constexpr auto Ly = 10.0;
 constexpr auto difussionCoefficient = 4.0;
-constexpr auto dt = 0.01;
+constexpr auto dt = 0.001;
 
 auto concentration(double t, double x, double y, size_t order) {
     double concentration = 0.0;
@@ -29,10 +29,12 @@ int main() {
     for (auto i = 0; i < ts.size(); ++i) ts[i] = i * dt;
 
     std::array<double, 200> xs;
-    for (auto i = 0; i < xs.size(); ++i) xs[i] = Ly * i / (double(xs.size()) - 1);
+    for (auto i = 0; i < xs.size(); ++i)
+        xs[i] = Lx * double(i) / double(xs.size() - 1);
 
     std::array<double, 100> ys;
-    for (auto i = 0; i < ys.size(); ++i) ys[i] = Ly * i / (double(ys.size()) - 1);
+    for (auto i = 0; i < ys.size(); ++i)
+        ys[i] = Ly * double(i) / double(ys.size() - 1);
 
     std::cout << "t,x,y,z,concentration" << std::endl;
     for (auto t : ts) for (auto x : xs) for (auto y : ys)
