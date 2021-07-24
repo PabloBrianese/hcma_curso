a7c9f773850124d1976fc1a78813c4d46b5e8f5e
PabloBrianese <pablobrianese@gmail.com>
2021-07-24 20:14:18 -0300

    using namespace std;

--- practica/_OF/ejercicio_2/fourierSeriesSolution/fourierSeriesSolution.cpp
+++ practica/_OF/ejercicio_2/fourierSeriesSolution/fourierSeriesSolution.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <array>
 
+using namespace std;
+
 constexpr auto order = 100;
 constexpr auto Lx = 20.0;
 constexpr auto Ly = 10.0;
@@ -25,20 +27,20 @@ auto concentration(double t, double x, double y, size_t order) {
 }
 
 int main() {
-    std::array<double, 20> ts;
+    array<double, 20> ts;
     for (auto i = 0; i < ts.size(); ++i) ts[i] = i * dt;
 
-    std::array<double, 200> xs;
+    array<double, 200> xs;
     for (auto i = 0; i < xs.size(); ++i)
         xs[i] = Lx * double(i) / double(xs.size() - 1);
 
-    std::array<double, 100> ys;
+    array<double, 100> ys;
     for (auto i = 0; i < ys.size(); ++i)
         ys[i] = Ly * double(i) / double(ys.size() - 1);
 
-    std::cout << "t,x,y,z,concentration" << std::endl;
+    cout << "t,x,y,z,concentration" << endl;
     for (auto t : ts) for (auto x : xs) for (auto y : ys)
-        std::cout
+        cout
             << t << "," << x << "," << y << "," << 0.5 <<  "," << concentration(t, x, y, order)
-            << std::endl;
+            << endl;
 }
